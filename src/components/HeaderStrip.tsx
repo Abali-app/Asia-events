01df3bdab2eeab5790d294d33dd01684068b8777
Abali-app <aisar.daghash@gmail.com>
2026-02-09 02:28:20 +0200

    Fix desktop image art direction (avoid strip crop)

--- src/components/HeaderStrip.tsx
+++ src/components/HeaderStrip.tsx
@@ -9,8 +9,14 @@ type HeaderStripProps = {
 
 export default function HeaderStrip({ src, alt = "" }: HeaderStripProps) {
   return (
-    <div className="relative h-[150px] w-full overflow-hidden sm:h-[200px]">
-      <Image src={src} alt={alt} fill className="object-cover" sizes="100vw" />
+    <div className="relative h-[160px] w-full overflow-hidden sm:h-[220px] md:h-[320px] lg:h-[420px]">
+      <Image
+        src={src}
+        alt={alt}
+        fill
+        className="object-cover object-top md:object-center"
+        sizes="100vw"
+      />
       <div className="absolute inset-0 bg-black/15" />
     </div>
   );
